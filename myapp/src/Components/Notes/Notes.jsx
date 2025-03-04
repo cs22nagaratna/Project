c2609d3bbe6e81ffdf17f72908812a203d2cdcbe
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-03-04 11:28:48 +0530

    frist commit

--- myapp/src/Components/Notes/Notes.jsx
+++ myapp/src/Components/Notes/Notes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Notes.css"
 import { GoBell } from "react-icons/go";
+import { TbUsersGroup } from "react-icons/tb";
 import { FaCalendarAlt } from "react-icons/fa";
 
 const Notes=()=>{
@@ -28,23 +29,60 @@ const Notes=()=>{
                     <div className="b1">
                         <div className="c1">
                             <h3>Design UI ToDo APP<br/><a>Friday ,03 July 2022</a></h3>
-                            <div className="im"></div>
+                            <div className="im">
+                            <TbUsersGroup />
+                            </div>
                         </div>
                         <p>Description</p>
-                        <a>Design a simple home pages with <br/><a></a>clean layout and color based on the <a>guidelines.</a></a>
+                        <a>Design a simple home pages with <br/><a></a>clean layout and color based on the <a>guidelines.</a></a><br/>
+                        <div className="cir">
+                            <div className="cir1">
+                                <div className="te">
+                                    <p>Terms:</p>
+                                </div>
+                                <div className="d1">
+                                    <div className="e1"></div>
+                                    <div className="e1"></div>
+                                    <div className="e1"></div>
+                                    <div className="e1"></div>
+                                </div>
+                            </div>
+                            <div className="cir2">
+                                <div className="pr">
+                                    <p>Progress:</p>
+                                </div>
+                                <div className="d2">
+                                    <div className="e2">
+                                        <div className="progress-circle"></div>
+                                    </div>
+                                    <div className="e3">78%</div>
+                                </div>
+                            </div>
+                        </div>
                         <div className="c3"></div>
                     </div>
                     <div className="b2">
-                    <div className="c2">
+                        <div className="c2">
                             <h3>Wee<br/><a>Friday</a></h3>
                         </div>
                         <p>Desc</p>
                         <a>Meet<br/><a></a>pages</a>
+                        <div className="ci">
+                            <div className="cir1">
+                                <div className="te">
+                                    <p>Terms:</p>
+                                </div>
+                                <div className="d1">
+                                    <div className="e1"></div>
+                                    <div className="e1"></div>
+                                </div>
+                            </div>
+                        </div>    
                         <div className="c4"></div>
                     </div>
                 </div>
                 <div className="third">
-                    <h3>Completed</h3>
+                <h3>Completed<a>View More</a></h3>
                 </div>
             </div>
             <div className="contant2"></div>
