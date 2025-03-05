8be7bda0a4cd1a0a3d957e31752d99ecdcef0b00
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-03-05 12:09:48 +0530

    frist commit

--- myapp/src/Components/Login/Login.jsx
+++ myapp/src/Components/Login/Login.jsx
@@ -3,7 +3,16 @@ import "./Login.css"
 
 const Login=()=>{
     return(
-        <div className="contaner"></div>
+        <div className="contaner">
+            <div className="boxes">
+                <img src="https://play-lh.googleusercontent.com/76gEFhQto5xMHr2Qf8nWLvm1s0O60clhkwHvxQDSeI3hthf7Zs05JJQeyg5H347DGQ" />
+                <h1>naukri</h1>
+                <p>Jobs</p>
+                <p>Companies</p>
+                <p>Services</p>
+                <div className="search"></div>
+            </div>
+        </div>
     );
 };
 export default Login;
