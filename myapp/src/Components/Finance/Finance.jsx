12b4934bc0210bfc6fc4f6eb5a1f732dbf5f08bc
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-03-06 12:22:47 +0530

    frist commit

--- myapp/src/Components/Finance/Finance.jsx
+++ myapp/src/Components/Finance/Finance.jsx
@@ -3,6 +3,8 @@ import "./Finance.css"
 import { FaStar } from "react-icons/fa6";
 import { IoCalendarClearOutline } from "react-icons/io5";
 import { SlLocationPin } from "react-icons/sl";
+import { GoDotFill } from "react-icons/go";
+import { CiStar } from "react-icons/ci";
 
 const Finance=()=>{
     return(
@@ -27,12 +29,59 @@ const Finance=()=>{
                 <div className="pos">
                     <h3>Posted: <a>17 days ago</a>  Openings: <a>2</a>   Application:<a>884</a></h3>
                     <div className="register">
-                        
+                        <h3>Register to apply</h3>
+                    </div>
+                    <div className="login">
+                        <h3>Login to apply</h3>
                     </div>
                 </div>
             </div>
             <div className="job">
-                
+                <h2>Job description</h2>
+                <div className="bengaluru">
+                    <h3>Unacademy - Bengalore</h3>
+                    <a>Position: Senior Finance Executive Revenue</a>
+                </div>
+                <div className="jobs">
+                    <h2>Job Description</h2>
+                    <p><GoDotFill /> Conduct a three-way revenue reconciliation (Revenue to Record).</p>
+                </div>
+                <div className="read">
+                    <h3>read more</h3>
+                </div>
+                <div className="key">
+                    <h2>Key Skills</h2>
+                    <p>Skills highlighted with '<CiStar />' are preferred keyskils</p>
+                </div>
+                <div className="star">
+                    <CiStar /> Ca
+                </div>
+                <div className="reco">
+                    <div className="revenue">
+                        <a>Revenue Reconciliation</a>
+                    </div>
+                    <div className="filing">
+                        <p>Gst Filing</p>
+                    </div>
+                    <div className="gst">
+                        <p>Gst</p>
+                    </div>
+                    <div className="recognition">
+                        <p>Revenue Recognition</p>
+                    </div>
+                </div>
+                <div className="border"></div>
+                <div className="website">
+                    <div className="facebook">
+                        <img src="https://static.vecteezy.com/system/resources/previews/021/495/985/non_2x/facebook-social-media-logo-icon-free-png.png"/>
+                    </div>
+                    <div className="x">
+                        <img src="https://icon2.cleanpng.com/20240119/phb/transparent-x-icon-black-and-white-x-in-the-1710888893456.webp"/>
+                    </div>
+                    <div className="in">
+                        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>
+                    </div>
+                </div>
             </div>
         </div>
     )
