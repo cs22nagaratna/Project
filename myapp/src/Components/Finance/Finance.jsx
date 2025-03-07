2cac31a464f6726cc5a23015fa4bac76e419c81b
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-03-07 10:04:25 +0530

    frist commit

--- myapp/src/Components/Finance/Finance.jsx
+++ myapp/src/Components/Finance/Finance.jsx
@@ -9,78 +9,89 @@ import { CiStar } from "react-icons/ci";
 const Finance=()=>{
     return(
         <div className="con">
-            <div className="finance">
-                <div className="senior">
-                    <div className="senior1">
-                        <h2>Senior  Finance  Executive  Revenue  To  Record  CA  Freshers</h2>
-                        <div className="unacademy">
-                            <h3>Unacademy  <a><FaStar /></a> 3.0  1945 Reviews</h3>
-                            <a><IoCalendarClearOutline />0.2 years<a>₹ Not Disclosed</a></a><br/>
-                            <a><SlLocationPin />Bengaluru</a>
+            <div className="left">
+                <div className="finance">
+                    <div className="senior">
+                        <div className="senior1">
+                            <h2>Senior  Finance  Executive  Revenue  To  Record  CA  Freshers</h2>
+                            <div className="unacademy">
+                                <h3>Unacademy  <a><FaStar /></a> 3.0  1945 Reviews</h3>
+                                <a><IoCalendarClearOutline />0.2 years<a>₹ Not Disclosed</a></a><br/>
+                                <a><SlLocationPin />Bengaluru</a>
+                            </div>
+                        </div>
+                        <div className="icon">
+                            <div className="icon1">
+                                <img src="https://play-lh.googleusercontent.com/DT0jm91vA_rOkxwatXhI4yVpjU4KI0augje6ZJGYxEyNjKvIhcQt8npiLHf5-ydclsqe" />
+                            </div>
+                            <h3>Send me jobs like this</h3>
                         </div>
                     </div>
-                    <div className="icon">
-                        <div className="icon1">
-                            <img src="https://play-lh.googleusercontent.com/DT0jm91vA_rOkxwatXhI4yVpjU4KI0augje6ZJGYxEyNjKvIhcQt8npiLHf5-ydclsqe" />
+                    <div className="pos">
+                        <h3>Posted: <a>17 days ago</a>  Openings: <a>2</a>   Application:<a>884</a></h3>
+                        <div className="register">
+                            <h3>Register to apply</h3>
                         </div>
-                        <h3>Send me jobs like this</h3>
+                        <div className="login">
+                            <h3>Login to apply</h3>
+                        </div>
                     </div>
-                </div>
-                <div className="pos">
-                    <h3>Posted: <a>17 days ago</a>  Openings: <a>2</a>   Application:<a>884</a></h3>
-                    <div className="register">
-                        <h3>Register to apply</h3>
                     </div>
-                    <div className="login">
-                        <h3>Login to apply</h3>
+                    <div className="job">
+                    <h2>Job description</h2>
+                    <div className="bengaluru">
+                        <h3>Unacademy - Bengalore</h3>
+                        <a>Position: Senior Finance Executive Revenue</a>
+                    </div>
+                    <div className="jobs">
+                        <h2>Job Description</h2>
+                        <p><GoDotFill /> Conduct a three-way revenue reconciliation (Revenue to Record).</p>
+                    </div>
+                    <div className="read">
+                        <h3>read more</h3>
+                    </div>
+                    <div className="key">
+                        <h2>Key Skills</h2>
+                        <p>Skills highlighted with '<CiStar />' are preferred keyskils</p>
+                    </div>
+                    <div className="star">
+                        <CiStar /> Ca
+                    </div>
+                    <div className="reco">
+                        <div className="revenue">
+                            <a>Revenue Reconciliation</a>
+                        </div>
+                        <div className="filing">
+                            <p>Gst Filing</p>
+                        </div>
+                        <div className="gst">
+                            <p>Gst</p>
+                        </div>
+                        <div className="recognition">
+                            <p>Revenue Recognition</p>
+                        </div>
+                    </div>
+                    <div className="border"></div>
+                    <div className="website">
+                        <div className="facebook">
+                            <img src="https://static.vecteezy.com/system/resources/previews/021/495/985/non_2x/facebook-social-media-logo-icon-free-png.png"/>
+                        </div>
+                        <div className="x">
+                            <img src="https://icon2.cleanpng.com/20240119/phb/transparent-x-icon-black-and-white-x-in-the-1710888893456.webp"/>
+                        </div>
+                        <div className="in">
+                            <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="job">
-                <h2>Job description</h2>
-                <div className="bengaluru">
-                    <h3>Unacademy - Bengalore</h3>
-                    <a>Position: Senior Finance Executive Revenue</a>
-                </div>
-                <div className="jobs">
-                    <h2>Job Description</h2>
-                    <p><GoDotFill /> Conduct a three-way revenue reconciliation (Revenue to Record).</p>
-                </div>
-                <div className="read">
-                    <h3>read more</h3>
-                </div>
-                <div className="key">
-                    <h2>Key Skills</h2>
-                    <p>Skills highlighted with '<CiStar />' are preferred keyskils</p>
-                </div>
-                <div className="star">
-                    <CiStar /> Ca
-                </div>
-                <div className="reco">
-                    <div className="revenue">
-                        <a>Revenue Reconciliation</a>
+            <div className="right">
+                <div className="roles"></div>
+                <div className="reviews">
+                    <div className="review">
+                        <h3>Reviews<a>Read all 1945 reviews</a></h3>
                     </div>
-                    <div className="filing">
-                        <p>Gst Filing</p>
-                    </div>
-                    <div className="gst">
-                        <p>Gst</p>
-                    </div>
-                    <div className="recognition">
-                        <p>Revenue Recognition</p>
-                    </div>
-                </div>
-                <div className="border"></div>
-                <div className="website">
-                    <div className="facebook">
-                        <img src="https://static.vecteezy.com/system/resources/previews/021/495/985/non_2x/facebook-social-media-logo-icon-free-png.png"/>
-                    </div>
-                    <div className="x">
-                        <img src="https://icon2.cleanpng.com/20240119/phb/transparent-x-icon-black-and-white-x-in-the-1710888893456.webp"/>
-                    </div>
-                    <div className="in">
-                        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>
-                    </div>
+                    
                 </div>
             </div>
         </div>
