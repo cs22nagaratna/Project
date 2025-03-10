476b6397a440cb59a430c12e5b5c378cb2eea7c2
cs22nagaratna <cs22nagaratna@svpkumta.in>
2025-03-10 10:24:15 +0530

    first commit

--- myapp/src/Components/Freshers/Freshers.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Freshers from "./Freshers"
-
-export default{
-    title:"Components/Freshers",
-    component:Freshers,
-};
-const Template=(args)=><Freshers {...args}/>
-export const freshers=Template.bind();
